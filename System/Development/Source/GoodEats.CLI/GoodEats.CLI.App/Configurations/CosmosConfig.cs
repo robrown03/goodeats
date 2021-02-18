65c47095b426a1ddc076d50010cfaa9d9e2cba39
robrown03 <robert.brown@outlook.com>
2021-02-18 10:22:31 -0500

    #5 Created CLI for loading data with instructions on running and query data source

---
+++ System/Development/Source/GoodEats.CLI/GoodEats.CLI.App/Configurations/CosmosConfig.cs
@@ -0,0 +1,34 @@
+﻿namespace GoodEats.CLI.Configurations
+{
+    public class CosmosConfig
+    {
+        /// <summary>
+        /// Gets or sets the account key.
+        /// </summary>
+        /// <value>
+        /// The account key.
+        /// </value>
+        public string AccountKey { get; set; }
+        /// <summary>
+        /// Gets or sets the endpoint URI.
+        /// </summary>
+        /// <value>
+        /// The endpoint URI.
+        /// </value>
+        public string EndpointUri { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the cosmos database.
+        /// </summary>
+        /// <value>
+        /// The name of the cosmos database.
+        /// </value>
+        public string DbName { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the cosmos database container.
+        /// </summary>
+        /// <value>
+        /// The name of the cosmos database container.
+        /// </value>
+        public string DbContainerName { get; set; }
+    }
+}
---
+++ System/Development/Source/GoodEats.CLI/GoodEats.CLI.App/Configurations/DataConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodEats.CLI.Configurations
+{
+    public class DataConfig
+    {
+        /// <summary>
+        /// Gets or sets the data path.
+        /// </summary>
+        /// <value>
+        /// The data path.
+        /// </value>
+        public string DataPath { get; set; }
+    }
+}
